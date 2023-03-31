317d6463dec547f53536e7128a9fefe02c14781d
Nelson Torres <ntorres381@gmail.com>
2023-03-31 15:59:38 -0700

    add variables to arithmetic solutions

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() * 10)  + 1;
+
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+
+const max = Math.max(17,18,19,20);
